90698d596cf861121dbb73a2a0f6ac685aff0d0a
forevercfc <144869452+forevercfc@users.noreply.github.com>
2024-01-08 17:21:22 +0900

    게시판 만들기

--- WebMarket_16th/src/main/java/mvc/controller/BoardController.java
+++ WebMarket_16th/src/main/java/mvc/controller/BoardController.java
@@ -13,45 +13,121 @@ import javax.servlet.http.HttpServletResponse;
 import mvc.model.BoardDAO;
 import mvc.model.BoardDTO;
 
-public class BoardController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    static final int LISTCOUNT = 5; // 한 페이지에 글 목록이 몇 개 보일지 설정
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // Implement your doPost logic here if needed.
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String requestURI = request.getRequestURI();
-        System.out.println("requestURI : " + requestURI);
-        String contextPath = request.getContextPath();
-        System.out.println("contextPath : " + contextPath);
-        String command = requestURI.substring(contextPath.length());
-
-        response.setContentType("text/html; charset=utf-8");
-
-        // Mapping
-        if (command.equals("/BoardListAction.do")) {
-            requestBoardList(request);
-            RequestDispatcher rd = request.getRequestDispatcher("./board/list.jsp");
-            rd.forward(request, response);
-        }
-    }
-
-    public void requestBoardList(HttpServletRequest request) {
-        BoardDAO dao = BoardDAO.getInstance();
-        List<BoardDTO> boardlist = new ArrayList<BoardDTO>();
-
-        int pageNum = 1;
-        int limit = LISTCOUNT;
-
-        if (request.getParameter("pageNum") != null) {
-            pageNum = Integer.parseInt(request.getParameter("pageNum"));
-        }
-        boardlist = dao.getBoardList(pageNum);
-        request.setAttribute("pageNum", pageNum);
-        request.setAttribute("boardlist", boardlist);
-    }
+public class BoardController extends HttpServlet{
+   private static final long serialVersionUID = 1L;
+   static final int LISTCOUNT = 5;//게시판 글목록이 한페이지에 몇개씩 보여질건지 상수로 지정
+   
+   protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+      doGet(req, res);
+   }
+   
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//      response.setHeader("Cache-Control", "no-cache");
+//      response.addHeader("Cache-Control", "no-store");
+//      response.setHeader("Pragma", "no-cache");
+//      response.setDateHeader("Expires", 1L);
+      
+      String RequestURI = request.getRequestURI();//가져오는 주소 /board/list.jsp
+      System.out.println("RequestURI :"  +RequestURI);
+      String contextPath = request.getContextPath();//가져오는 주소 /board
+      System.out.println("contextPath :" +contextPath);
+      String command = RequestURI.substring(contextPath.length()); //가져오는 주소 /list.jsp
+//       예)/board/list.jsp=>/list.jsp
+            
+      response.setContentType("text/html; charset=utf-8");
+      request.setCharacterEncoding("utf-8");
+      
+//      mapping
+      if(command.equals("/BoardListAction.do")) {
+         requestBoardList(request); // 게시판 목록 가져오는 사용자 정의 함수
+         RequestDispatcher rd = request.getRequestDispatcher("./board/list.jsp");
+         rd.forward(request, response);
+      }else if(command.equals("/BoardWriteForm.do")) { //글 등록 페이지로의 요청을 받으면
+         requestLoginName(request); //인증된 사용자명 가져오는 사용자 정의 함수
+         RequestDispatcher rd = request.getRequestDispatcher("./board/writeForm.jsp");
+         rd.forward(request, response);//request, response 매개변수를 가지고 디스패쳐 주소로 이동
+      }else if(command.equals("/BoardWriteAction.do")) {
+         requestBoardWrite(request);
+         RequestDispatcher rd = request.getRequestDispatcher("BoardListAction.do");
+         rd.forward(request, response);//request, response 매개변수를 가지고 디스패쳐 주소로 이동
+      }
+   }
+   
+//   등록된 글 목록을 가져오는 함수
+   public void requestBoardList(HttpServletRequest request) {
+      BoardDAO dao = BoardDAO.getInstance();
+      List<BoardDTO> boardlist = new ArrayList<BoardDTO>();
+      
+      int pageNum = 1;
+      int limit = LISTCOUNT;
+      
+      if(request.getParameter("pageNum") != null) {
+         pageNum = Integer.parseInt(request.getParameter("pageNum"));
+      }
+      
+      
+      //사용자가 게시판 하단의 검색창을 이용해서 option(items)를 선택하고 문자열(text)을 입력후 검색버튼을 클릭하며 request 객체에 저장하여 얻어와 해당 변수에 초기화
+      String items = request.getParameter("items");
+      String text = request.getParameter("text");
+      
+      //옵션별로 검색한 단어와 관련된 갯수를 리턴해서 total_record에 저장
+      int total_record = dao.getListCount(items, text);      
+      
+      boardlist = dao.getBoardList(pageNum,limit,items,text);
+      
+      request.setAttribute("pageNum", pageNum);
+      request.setAttribute("total_record", total_record);
+      
+      int total_page; //총 게시판 페이지수
+      //조건에 맞는 게시글 행을 limit(5)로 나누어 나머지가 0이면 그냥 몫을 게시판 페이지수로 설정
+      if(total_record % limit == 0) total_page = total_record/limit;
+      else {
+         total_page = total_record/limit;
+         total_page = total_page+1;
+      }//나머지가 0이 아니면 총 게시판 페이지수에 1을 더해 저장한다.
+      
+      request.setAttribute("total_page", total_page);
+      request.setAttribute("boardlist", boardlist);
+   }
+   
+   //인증된 사용자명 가져오는 함수 구현
+   public void requestLoginName(HttpServletRequest request) {
+      String id = request.getParameter("id");
+      BoardDAO dao = BoardDAO.getInstance();
+      String name = dao.getLoginNameById(id);
+      request.setAttribute("name", name);
+   }
+   
+   //새로운 글 등록하는 함수구현
+   public void requestBoardWrite(HttpServletRequest request) {
+      BoardDAO dao = BoardDAO.getInstance();
+      
+      BoardDTO board = new BoardDTO();
+      board.setId(request.getParameter("id"));
+      board.setName(request.getParameter("name"));
+      board.setSubject(request.getParameter("subject"));
+      board.setContent(request.getParameter("content"));
+      
+      System.out.println(request.getParameter("id"));
+      System.out.println(request.getParameter("name"));
+      System.out.println(request.getParameter("subject"));
+      System.out.println(request.getParameter("content"));
+      
+      board.setHit(0);
+      board.setIp(request.getRemoteAddr());
+      
+      dao.insertBoard(board);
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 }
--- WebMarket_16th/src/main/webapp/board/list.jsp
+++ WebMarket_16th/src/main/webapp/board/list.jsp
@@ -1,61 +1,127 @@
-<%@page import="java.util.*"%>
-<%@page import="mvc.model.BoardDTO"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+<%@ page import="java.util.*"%>
+<%@ page import="mvc.model.BoardDTO"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ include file="/menu.jsp"%>
 <%
-/* String sessionId = (String) session.getAttribute("sessionId"); */
+/* String sessionId = (String)session.getAttribute("sessionId"); */
+/* response.setContentType("text/html; charset=utf-8");
+request.setCharacterEncoding("utf-8"); */
 int pageNum = (Integer) request.getAttribute("pageNum");
-List<BoardDTO> boardList = (List<BoardDTO>) request.getAttribute("boardlist");
+List boardList = (List) request.getAttribute("boardlist");
+int total_record = (Integer) request.getAttribute("total_record");
+int total_page = (Integer) request.getAttribute("total_page");
+String items = (String) request.getAttribute("items");
+String text = (String) request.getAttribute("text");
 %>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <title>게시판</title>
+<meta charset="UTF-8">
+<title>게시판</title>
+<script>
+   function checkForm(){
+      if(${sessionId==null}){
+         alert("로그인 해주세요");
+         location.href="./member/loginMember.jsp";
+         return false
+      }
+      location.href = "./BoardWriteForm.do?id=<%=sessionId%>";
+   }
+</script>
 </head>
 <body>
-    <%@ include file="/menu.jsp"%>
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col px-0">
+	<div class="container-fluid text-center">
+		<div class="row">
+			<div class="col px-0">
+				<div class="jumbotron jumbotron-fluid">
+					<div class="container text-center">
+						<h1 class="display-4">게시판</h1>
+					</div>
+				</div>
+				<form action="<c:url value="./BoardListAction.do?"/>" method="post">
+					<div class="container table-responsive">
 
-                <div class="jumbotron jumbotron-fluid">
-                    <div class="container text-center">
-                        <h1 class="display-4">게시판</h1>
-                    </div>
-                </div>
-                <div class="container table-responsive">
-                    <table class="table">
-                        <tr>
-                            <th>번호</th>
-                            <th>제목</th>
-                            <th>작성일</th>
-                            <th>수정일</th>
-                            <th>조회수</th>
-                            <th>글쓴이</th>
-                        </tr>
-                        <%
-                            for(int i = 0; i < boardList.size(); i++) {
-                                BoardDTO boardDTO = boardList.get(i);
-                        %>
-                        <tr>
-                            <td><%= boardDTO.getNum() %></td>
-                            <td><%= boardDTO.getSubject() %></td>
-                            <td><%= boardDTO.getRegist_day() %></td>
-                            <td><%= boardDTO.getUpdate_day() %></td>
-                            <td><%= boardDTO.getHit() %></td>
-                            <td><%= boardDTO.getName() %></td>
-                        </tr>
-                        <%
-                            }
-                        %>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-    <%@ include file="/footer.jsp"%>
+						<div class="text-right">
+							<span>전체 <%=total_record%>건
+							</span>
+						</div>
+						<table class="table">
+							<tr>
+								<th>번호</th>
+								<th>제목</th>
+								<th>작성일</th>
+								<th>수정일</th>
+								<th>조회</th>
+								<th>글쓴이</th>
+							</tr>
+							<%
+							for (int i = 0; i < boardList.size(); i++) {
+								BoardDTO boardDTO = (BoardDTO) boardList.get(i);
+							%>
+							<tr>
+								<td><%=boardDTO.getNum()%></td>
+								<td><a
+									href="./BoardViewAction.do?num=<%=boardDTO.getNum()%>&pageNum=<%=pageNum%>"><%=boardDTO.getSubject()%></a>
+								</td>
+								<td><%=boardDTO.getRegist_day()%></td>
+								<td><%=boardDTO.getUpdate_day()%></td>
+								<td><%=boardDTO.getHit()%></td>
+								<td><%=boardDTO.getName()%></td>
+							</tr>
+							<%
+							}
+							%>
+						</table>
+					</div>
+					<!-- container -->
+					<!-- 페이징 구조 추가 -->
+					<div class="text-center">
+						<c:set var="pageNum" value="<%=pageNum%>" />
+						<c:set var="items" value="<%=items%>" />
+						<c:set var="text" value="<%=text%>" />
+						<c:forEach var="i" begin="1" end="<%=total_page%>">
+							<%-- <a href="<c:url value="./BoardListAction.do?pageNum=${i}&items='+${items}+'&text='+${text}+'"/>"> --%>
+							<a href="<c:url value='./BoardListAction.do?pageNum=${i}' />">
+								<c:choose>
+									<c:when test="${pageNum==i}">
+										<b>[${i}]</b>
+									</c:when>
+									<c:otherwise>
+                           [${i}]
+                        </c:otherwise>
+								</c:choose>
+							</a>
+						</c:forEach>
+					</div>
+					<!-- 검색 기능 구조 추가 -->
+					<div class="text-left container">
+						<table class="col-12">
+							<tr>
+								<td align="left" class="col-12 col-sm-8"><select
+									name="items" id="" class="control">
+										<option value="subject">제목에서</option>
+										<option value="content">본문에서</option>
+										<option value="name">글쓴이에서</option>
+								</select> <input type="text" name="text" /> <input type="submit"
+									class="btn btn-primary" value="검색" /></td>
+								<td align="right" class="col-12 col-sm-4"><a href="#"
+									onclick="checkForm(); return false;" class="btn btn-warning">&laquo;글쓰기</a>
+								</td>
+							</tr>
+						</table>
+					</div>
+				</form>
+			</div>
+		</div>
+		<!-- row -->
+	</div>
+	<%@ include file="/footer.jsp"%>
 </body>
 </html>
+
+
+
